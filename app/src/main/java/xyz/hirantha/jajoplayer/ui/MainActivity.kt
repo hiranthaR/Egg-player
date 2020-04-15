e080b8f0422cc877d1d5f02811381a8d31cddfd7
hirantha <sahanhirantha@gmail.com>
2020-04-15 22:55:54 +0530

    navigation graph created

--- app/src/main/java/xyz/hirantha/jajoplayer/ui/MainActivity.kt
+++ app/src/main/java/xyz/hirantha/jajoplayer/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package xyz.hirantha.jajoplayer
+package xyz.hirantha.jajoplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.hirantha.jajoplayer.R
 
 class MainActivity : AppCompatActivity() {
 
